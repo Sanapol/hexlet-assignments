9f102e80b337bc57978690e63b29e54123d0c2ac
@hexlet/cli <support@hexlet.io>
2025-01-20 12:14:36 +0300

    submit java-web-ru/http-session

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -18,13 +18,13 @@ public final class App {
 
         // BEGIN
         app.get("/users", ctx -> {
-           var page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
-           var per = ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
-           List<Map<String, String>> result = new ArrayList<>();
-           for (var i = per * page - per; i < per * page; i++) {
+            var page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+            var per = ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
+            List<Map<String, String>> result = new ArrayList<>();
+            for (var i = per * page - per; i < per * page; i++) {
                result.add(USERS.get(i));
-           }
-           ctx.json(result);
+            }
+            ctx.json(result);
         });
         // END
 
